06d45b75254d065bc285d9bda7f41ac6211bb3d8
Gizachew Bayness <eleccrazy24@gmail.com>
2022-08-13 07:56:53 -0700

    Fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,25 +14,27 @@ void selection_sort(int *array, size_t size)
 	size_t i = 0, j, index;
 	int min, status = 0;
 
-	for (; i < size - 1; i++)
-	{
-		status = 0;
-		min = array[i];
-		for (j = i; j < size; j++)
+	if (size > 1)
+	{	for (; i < size - 1; i++)
 		{
-
-			if (array[j] < min)
+			status = 0;
+			min = array[i];
+			for (j = i; j < size; j++)
 			{
-				status = 1;
-				min = array[j];
-				index = j;
+
+				if (array[j] < min)
+				{
+					status = 1;
+					min = array[j];
+					index = j;
+				}
 			}
-		}
-		if (status == 1)
-		{
-			array[index] = array[i];
-			array[i] = min;
-			print_array(array, size);
+			if (status == 1)
+			{
+				array[index] = array[i];
+				array[i] = min;
+				print_array(array, size);
+			}
 		}
 	}
 }
