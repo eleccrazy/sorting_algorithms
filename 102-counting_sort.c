afa438172c6d7bae25945f9a8389d61e66b7ea6c
Gizachew Bayness <eleccrazy24@gmail.com>
2022-08-14 06:06:25 -0700

    Bug Fixed

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -34,13 +34,10 @@ void counting_sort(int *array, size_t size)
 	for (j = 1; j <= max; j++)
 		counter[j] = counter[j] + counter[j - 1];
 
-	for (i = 0; i < size; i++)
-	{
-		temp[counter[array[i]] - 1] = array[i];
-		counter[array[i]] -= 1;
-	}
-
 	print_array(counter, max + 1);
+
+	for (i = 0; i < size; i++)
+		temp[counter[array[i] - 1]] = array[i];
 
 	for (i = 0; i < size; i++)
 		array[i] = temp[i];
