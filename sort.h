e4cd9728df59905426de1fe8aa5c2418623bc7d9
Gizachew Bayness <eleccrazy24@gmail.com>
2022-08-14 03:56:25 -0700

    Header file updated

--- sort.h
+++ sort.h
@@ -41,4 +41,13 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
 #endif
