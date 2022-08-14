e20962bc333dcbbbd9bdaf081675c5ebd996a9c8
Gizachew Bayness <eleccrazy24@gmail.com>
2022-08-14 00:49:24 -0700

    task 3

---
+++ 3-quick_sort.c
@@ -0,0 +1,89 @@
+#include "sort.h"
+
+/**
+ * swapper - Swappes two elements of the array.
+ *
+ * @array: The array to be sorted.
+ * @a: The first index to be swapped.
+ * @b: The second index to be swapped.
+ *
+ */
+
+void swapper(int *array, int a, int b)
+{
+	int temp = array[a];
+
+	array[a] = array[b];
+	array[b] = temp;
+}
+
+/**
+ * pivot_finder - Finds the pivot from the array.
+ *
+ * @array: The array to be sorted.
+ * @l: The lowest index
+ * @h: The higherst index.
+ *
+ * Return: Returns the pivot index from the array.
+ *
+ */
+
+int pivot_finder(int *array, int l, int h)
+{
+	int p_value = array[h];
+	int i = l, j;
+
+	for (j = l; j < h; j++)
+	{
+		if (array[j] <= p_value)
+		{
+			swapper(array, i, j);
+			i++;
+		}
+	}
+
+	swapper(array, i, h);
+
+	return (i);
+}
+
+/**
+ * recursive_helper - helps the quick_sort function by applying
+ * recursive checking on the partitioned array.
+ *
+ * @array: The array to be sorted
+ * @low: The lowest index from the pivot index.
+ * @high: The higer index from the pivot index.
+ * @size: The size of the array.
+ *
+ */
+
+void recursive_helper(int *array, int low, int high, size_t size)
+{
+	int p_index;
+
+	if (low < high)
+	{
+		print_array(array, size);
+		p_index = pivot_finder(array, low, high);
+		recursive_helper(array, low, p_index - 1, size);
+		recursive_helper(array, p_index + 1, high, size);
+	}
+	else
+		return;
+}
+
+/**
+ * quick_sort - sorts an array of integers in ascending
+ * order using the Quick sort algorithm
+ *
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ *
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	recursive_helper(array, 0, size - 1, size);
+	print_array(array, size);
+}
