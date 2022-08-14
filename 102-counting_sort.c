d1eed3683ed393a28be7c4bde0ecf325904962bc
Gizachew Bayness <eleccrazy24@gmail.com>
2022-08-14 05:34:17 -0700

    task 6

---
+++ 102-counting_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+#include <stdio.h>
+/**
+ * counting_sort - Sorts an array of integers in ascending
+ * order using the Counting sort algorithm
+ *
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ *
+ */
+
+void counting_sort(int *array, size_t size)
+{
+	size_t i = 1;
+	int max = array[0], j, index;
+	int *counter, *temp;
+
+	for (; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+
+	counter = malloc(sizeof(int) * max + 1);
+	temp = malloc(sizeof(int) * size);
+
+	for (i = 0; i < size; i++)
+		counter[array[i]] += 1;
+
+	for (j = 1; j <= max; j++)
+		counter[j] = counter[j] + counter[j - 1];
+
+	for (i = 0; i < size; i++)
+	{
+		index = array[i];
+		temp[counter[index - 1]] = index;
+	}
+
+	print_array(counter, max + 1);
+
+	for (i = 0; i < size; i++)
+		array[i] = temp[i];
+
+	free(temp);
+	free(counter);
+}
